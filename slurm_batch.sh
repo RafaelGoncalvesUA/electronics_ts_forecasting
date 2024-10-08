556dc1f2ec6877f9f2f9a56773b3bce3c8168290
Rafael Fernandes Gonçalves <rfg@ua.pt>
2024-10-08 14:00:46 +0100

    Update batch slurm

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=benchmark.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1                  # 1 tarefa (processo) por nó
-#SBATCH --cpus-per-task=1           # 1 CPU por tarefa
+#SBATCH --cpus-per-task=8           # 8 CPUs por tarefa
 #SBATCH --gres=gpu:1                # 1 GPU por nó
 #SBATCH --time=7-00:00:00           # Limite de tempo para execução
 #SBATCH --partition=gpuPartition    # Partição padrão (ajustar conforme necessário)
